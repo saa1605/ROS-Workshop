71d45208686fdb9028460ef95af785041e9f197e
saa1605 <saaket.agashe@gmail.com>
2018-03-31 16:32:19 +0530

    made into catkin_ws

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/saaket/ROS-Workshop/src
+# Build directory: /home/saaket/ROS-Workshop/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
